860d30b4b5f9e91377375d5e5c9c44d30d449647
AbiSalami <bimwealth215@gmail.com>
2023-10-11 11:31:41 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - addition of two digits
+ * @a: value to add
+ * @b:bvalue to add
+ *
+ * Return: c as the output
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
