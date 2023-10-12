1a20eb6a3b952fb3a67493adf0e7c9a11d621cbe
AbiSalami <bimwealth215@gmail.com>
2023-10-12 10:56:50 -0700

    task 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include "main.h"
-
+/* betty style doc for function main goes there */
 /**
  * positive_or_negative - entry point
+ * @i: parameter for int i
  * Return: 0 (success)
 */
 void positive_or_negative(int i)
@@ -13,5 +13,4 @@ void positive_or_negative(int i)
 		printf("%d is zero\n", i);
 	else
 		printf("%d is negative\n", i);
-	return;
 }
