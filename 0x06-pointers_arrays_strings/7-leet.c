c6c13f3b31b48ad502fbc59076fcb50a22c67985
AbiSalami <bimwealth215@gmail.com>
2023-10-20 13:56:22 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,11 +12,11 @@ char *leet(char *s)
 	char *a = "aAeEoOtTlL";
 	char *b = "4433007711";
 
-	for (i = 0 s[i] != '\0' ; i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 	{
 		for (j = 0 ; j < 10 ; j++)
 		{
-			if s([i] == a[j])
+			if (s[i] == a[j])
 				s[i] = b[j];
 		}
 	}
