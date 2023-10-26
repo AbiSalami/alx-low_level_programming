79b223d97e93f7045a4af06af3a0dd022e3f14fc
AbiSalami <bimwealth215@gmail.com>
2023-10-25 17:15:09 -0700

    pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _pow-recursion - function that returns the value of x raised to the power of y
+ * _pow-recursion - function that returns the value
+ *                  of x raised to the power of y
  * @x: value to raise
  * @y: power
  * Return: result of the power
